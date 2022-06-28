bc3e0157fa2a0c0d23fe2938460fe684b412cdc6
Kamesh <kamesh.sampath@hotmail.com>
2022-06-28 09:09:20 +0530

    (fix): download and cache plugin json

--- src/plugins/pluginApi.ts
+++ src/plugins/pluginApi.ts
@@ -3,11 +3,15 @@
  *  Licensed under the MIT License. See LICENSE file in the project root for license information.
  *-----------------------------------------------------------------------------------------------*/
 
+import * as vscode from 'vscode';
 import * as fsex from 'fs-extra';
 import * as _ from 'lodash';
 import * as path from 'path';
 import { Platform } from '../util/platform';
 import { getInstallFolder } from '../util/settings';
+import * as moment from 'moment';
+import { Errorable, failed } from '../errorable';
+import * as axios from 'axios';
 
 export interface DronePluginSettings{
 	title: string
@@ -29,15 +33,41 @@ export interface DronePlugin {
 	url: string
 }
 
-//TODO get the plugins.json path from settings
 export async function searchPluginsByNameOrTags(...searchTerms): Promise<DronePlugin[]>{
   let matchedPlugins = new Array<DronePlugin>;
+  let dronePlugins = new Array<DronePlugin>();
+
   const homePath = Platform.getUserHomePath();
   if (homePath){
-    const pluginJsonPath = path.join(getInstallFolder(),'plugins','plugins.json');
+    const pluginsCacheFile = path.join(getInstallFolder(),'plugins','plugins.json');
 
-    const dronePlugins: DronePlugin[] = await fsex.readJSON(pluginJsonPath);
+    const pluginsFileExists = await fsex.pathExists(pluginsCacheFile);
 
+    let pluginCacheResult:Errorable<any>;
+    
+    if (!pluginsFileExists){
+      await fsex.ensureFile(pluginsCacheFile);
+      pluginCacheResult = await getAndCachePlugins(pluginsCacheFile);
+    } else {
+      const stats = await fsex.stat(pluginsCacheFile);
+      if (refreshCache(stats)){
+        await fsex.remove(pluginsCacheFile);
+        pluginCacheResult = await getAndCachePlugins(pluginsCacheFile);
+      } else {
+        pluginCacheResult = { succeeded: true, result: await fsex.readJSON(pluginsCacheFile) };
+      }
+    }
+
+    if (failed(pluginCacheResult)){
+      const cacheReq = await vscode.window.showErrorMessage('Error caching plugins data','OK','Try again');
+      do {
+        pluginCacheResult = await getAndCachePlugins(pluginsCacheFile);
+      } while (cacheReq === 'Try again'); 
+      return [];
+    } 
+    
+    dronePlugins = pluginCacheResult.result;
+  
     if (searchTerms && searchTerms.length > 0){
       matchedPlugins = _.filter(dronePlugins, (o) =>{
         let isMatched = false;
@@ -54,4 +84,28 @@ export async function searchPluginsByNameOrTags(...searchTerms): Promise<DronePl
   }
   
   return matchedPlugins;
+}
+
+async function getAndCachePlugins(releaseCacheFile: string
+): Promise<Errorable<any>> {
+  try {
+    const res = await axios.default
+      .get('https://plugins.drone.io/api/plugins');
+    await fsex.writeJson(releaseCacheFile, res.data);
+    return { succeeded: true, result: res.data };
+  } catch (e){
+    return {
+      succeeded: false,
+      error: [
+        `Failed to cache plugins info : ${e}`,
+      ],
+    };
+  }
+}
+
+//Check if the file is 1 hour old
+function refreshCache(stats: fsex.Stats): boolean {
+  const modifiedTime = stats.mtimeMs;
+  const days = moment().diff(moment(modifiedTime),'hour');
+  return days > 1;
 }
